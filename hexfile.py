1a6da435a965a8d84765e639517d12456a936dc5
risapav <risapav@gmail.com>
2020-01-13 18:51:51 +0100

    added comments

--- hexfile.py
+++ hexfile.py
@@ -11,12 +11,12 @@ import struct
 import codecs
 
 
-ROWTYPE_DATA = 0x00
-ROWTYPE_EOF = 0x01
-ROWTYPE_EXT_SEG_ADDR = 0x02
-ROWTYPE_START_SEG_ADDR = 0x03
-ROWTYPE_EXT_LIN_ADDR = 0x04
-ROWTYPE_START_LIN_ADDR = 0x05
+ROWTYPE_DATA = 0x00  # Data container
+ROWTYPE_EOF = 0x01  # End of file
+ROWTYPE_EXT_SEG_ADDR = 0x02  # Extended Segment Address
+ROWTYPE_START_SEG_ADDR = 0x03  # Start Segment Address
+ROWTYPE_EXT_LIN_ADDR = 0x04  # Extended Linear Address
+ROWTYPE_START_LIN_ADDR = 0x05   # Start Linear Address
 
 
 class HexFile:
@@ -25,17 +25,20 @@ class HexFile:
     """
 
     def __init__(self, filename):
+        # nazov suboru vo formate intel hex
         self._filename = filename
+        # nedolezite udaje z pohladu umistnenia dat v pamati
         self._CS = 0
         self._IP = 0
         self._EIP = 0
-
+        # udaje dolezite pre vypocet umiestnenia v pamati
         self._ADDRESS = 0
         self._SBA = 0
         self._LBA = 0
 
         self._typ = ROWTYPE_DATA
 
+    # spustenie analyzy intel hex suboru
     def doAnalyze(self):
         with open(self._filename, 'r', encoding='utf-8') as fp:
             cnt = 1
@@ -50,15 +53,13 @@ class HexFile:
                     continue
                 # ------------------------------------------------------------
                 # vypocet dlzky retazca ihex recordu
-                buffer = codecs.decode(line[1:3], "hex")
-                dataend = struct.unpack_from(">B", buffer, offset=0)[0]
-                dataend *= 2
-                dataend += 2 + 4 + 2 + 2 + 1
+                data = self.byteCnv(line[1:3])
+                # 1[:] + 2[LL] + 4[AAAA] + 2[TT] + 2n[DATA] + 2[CC]
+                dataend = 1 + 2 + 4 + 2 + 2*data + 2
                 # print(line[0:dataend])
                 # ------------------------------------------------------------
                 # crc vypocitane zo zvysku riadku musi byt 0
                 crc = self.calcChecksum(line[1:dataend])
-                # print("crc : ", crc)
                 if crc != 0:
                     raise ValueError(
                         "Record checksum doesn't match on line %d" % cnt)
@@ -72,34 +73,44 @@ class HexFile:
                 self.analyzeLine(typ, length, addr, data)
                 cnt += 1
 
-    def setAddress(self, DRLO):
+    # nastavenie adresy umiestnenia dat
+    # drlo - adresa Word
+    def setAddress(self, drlo):
+        # index
+        dri = 0
         if self._typ == ROWTYPE_EXT_SEG_ADDR:  # Extended Segment Address
-            DRI = 0
-            self._ADDRESS = self._SBA * 0x10 + (DRLO + DRI) % 0xFFFF
+            self._ADDRESS = self._SBA * 0x10 + (drlo + dri) % 0xFFFF
 
         elif self._typ == ROWTYPE_EXT_LIN_ADDR:  # Extended Linear Address
-            DRI = 0
-            self._ADDRESS = (self._LBA * 0x10000 + DRLO + DRI) % 0xFFFFFFFF
+            self._ADDRESS = (self._LBA * 0x10000 + drlo + dri) % 0xFFFFFFFF
 
         else:
-            DRI = 0
-            self._ADDRESS = DRLO + DRI
+            self._ADDRESS = drlo + dri
 
+    # konverzia z textoveho stringu na cislo velkosti Byte
+    # data - textovy retazec data 2 znaky
     def byteCnv(self, data):
         buffer = codecs.decode(data, "hex")
-        return struct.unpack(">B", buffer)[0]
+        return struct.unpack(">B", buffer[0:1])[0]
 
+    # konverzia z textoveho stringu na cislo velkosti Word
+    # data - textovy retazec data 4 znaky
     def wordCnv(self, data):
         buffer = codecs.decode(data, "hex")
-        num = struct.unpack(">H", buffer[0:2])[0]
-        # print("wordCnv : ", data, buffer, num)
-        # print("wordCnv : ", type(data), type(buffer), type(num))
-        return num
+        return struct.unpack(">H", buffer[0:2])[0]
 
+    # konverzia z textoveho stringu na cislo velkosti DWord
+    # data - textovy retazec data 8 znakov
     def dwordCnv(self, data):
         buffer = codecs.decode(data, "hex")
-        return struct.unpack(">I", buffer)[0]
+        return struct.unpack(">I", buffer[0:4])[0]
 
+    # textový výpis do stdout
+    # typ - typ zaznamu 0-5
+    # length - dlzka datovej casti
+    # addr - nacitana adresa (index)
+    # data - textovy retazec data
+    # txt - komentar
     def txtMessage(self, typ, length, addr, data, txt):
         print('{0:0{1}X}'.format(self._ADDRESS, 8),
               "typ:", '{0:0{1}X}'.format(typ, 2),
@@ -109,21 +120,23 @@ class HexFile:
               " -> ", txt
               )
 
-    # print(riadok[:-1])
+    # analyzovanie parsovaneho riadku
+    # typ - typ zaznamu 0-5
+    # length - dlzka datovej casti
+    # addr - nacitana adresa (index)
+    # data - textovy retazec data
     def analyzeLine(self, typ, length, addr, data):
-        buffer = codecs.decode(data[:], "hex")
-
-        if typ == ROWTYPE_DATA:  # Data container
+        if typ == ROWTYPE_DATA:  # Data container 0x00
             self.setAddress(addr)
             self.txtMessage(typ, length, addr, data, " data ")
 
-        elif typ == ROWTYPE_EOF:  # End of file
+        elif typ == ROWTYPE_EOF:  # End of file 0x01
             # print("End of file")  # Should we check for garbage after this?
             self._typ = ROWTYPE_DATA
             self.setAddress(addr)
             self.txtMessage(typ, length, addr, data, "End of file")
 
-        elif typ == ROWTYPE_EXT_SEG_ADDR:  # Extended Segment Address
+        elif typ == ROWTYPE_EXT_SEG_ADDR:  # Extended Segment Address 0x02
             # SBA +  ([DRLO  +  DRI]  MOD  64K)
             self._typ = ROWTYPE_DATA
             self.setAddress(addr)
@@ -132,7 +145,7 @@ class HexFile:
             self._SBA = self.wordCnv(data)
             self._typ = ROWTYPE_EXT_SEG_ADDR
 
-        elif typ == ROWTYPE_START_SEG_ADDR:  # Start Segment Address
+        elif typ == ROWTYPE_START_SEG_ADDR:  # Start Segment Address 0x03
             # CS:IP
             self._typ = ROWTYPE_DATA
             self.setAddress(addr)
@@ -141,7 +154,7 @@ class HexFile:
             self._CS = self.wordCnv(data[0:2])
             self._IP = self.wordCnv(data[2:4])
 
-        elif typ == ROWTYPE_EXT_LIN_ADDR:  # Extended Linear Address
+        elif typ == ROWTYPE_EXT_LIN_ADDR:  # Extended Linear Address 0x04
             # (LBA + DRLO + DRI) MOD 4G
             self._typ = ROWTYPE_DATA
             self.setAddress(addr)
@@ -150,7 +163,7 @@ class HexFile:
             self._LBA = self.wordCnv(data)
             self._typ = ROWTYPE_EXT_LIN_ADDR
 
-        elif typ == ROWTYPE_START_LIN_ADDR:  # Start Linear Address
+        elif typ == ROWTYPE_START_LIN_ADDR:  # Start Linear Address 0x05
             # EIP
             self._typ = ROWTYPE_DATA
             self.txtMessage(typ, length, addr,
@@ -160,6 +173,8 @@ class HexFile:
         else:  # undefined record
             raise ValueError("Invalid type byte")
 
+    # vypocet crc suctu
+    # data - textovy retazec data
     def calcChecksum(self, data):
         crc = 0
         buffer = codecs.decode(data, "hex")
@@ -168,17 +183,26 @@ class HexFile:
             crc += byte
         return crc & 0xFF
 
+    # parsovanie jedne nacitaneho riadku
+    # cnt - cislo nacitaneho riadku
+    # rawline - textovy string, jeden riadok zo suboru
     def parseLine(self, cnt, rawline):
         try:
-            line = codecs.decode(rawline, "hex")
-            # print(cnt, rawline, line)
-            length, addr, typ = struct.unpack_from(">BHB", line, offset=0)
+            # dlzka dat v zazname
+            length = self.byteCnv(rawline[0:2])
+            # adresa umiestnenia
+            addr = self.wordCnv(rawline[2:6])
+            # typ zaznamu
+            typ = self.byteCnv(rawline[6:8])
+            # data zaznamu
             data = rawline[8:]
             return (typ, length, addr, data)
         except ValueError:
             raise ValueError("Invalid hex data")
 
         return (0x00, 0x00, 0x00, "\xFF\xFF")
+
+# hlavna funkcia
 
 
 def main():
@@ -187,4 +211,5 @@ def main():
     return 0
 
 
+# spustenie programu
 main()
